c68920430ca0722d663d74c118cd501a5debb9eb
hoangphatnguyen <55780173+hoangphatnguyen@users.noreply.github.com>
2019-12-09 12:25:55 +0700

    vi déo

--- src/java/com/poly/dao/VideoDAO.java
+++ src/java/com/poly/dao/VideoDAO.java
@@ -5,10 +5,10 @@
  */
 package com.poly.dao;
 
-import com.poly.bean.Banner;
 import com.poly.bean.Video;
 import com.poly.tool.ConstantManager;
 import java.util.List;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class VideoDAO {
+
     @Autowired
     JdbcTemplate jdbc;
 
@@ -31,8 +32,66 @@ public class VideoDAO {
     private RowMapper<Video> getRowMapper() {
         return new BeanPropertyRowMapper<>(Video.class);
     }
-     public List<Video> getVideo(){
-        String sql = "SELECT * FROM "+ConstantManager.DEFAULT_DB_NAME+".video ORDER BY created_time";
+
+    public List<Video> getVideo() {
+        String sql = "SELECT * FROM " + ConstantManager.DEFAULT_DB_NAME + ".video ORDER BY created_time";
+        return getBySql(sql);
+    }
+
+    public Boolean insert(Video v) {
+        try {
+            String sql = "INSERT into video(id, link , title)"
+                    + "VALUES(?,?,?)";
+            jdbc.update(sql, v.getId(), v.getLink(), v.getTitle());
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Boolean.FALSE;
+        }
+    }
+
+    public Boolean delete(String id) {
+        try {
+            String sql = "DELETE FROM " + ConstantManager.DEFAULT_DB_NAME + ".video "
+                    + "WHERE id=?";
+            jdbc.update(sql, id);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Boolean.FALSE;
+        }
+    }
+
+    public Boolean update(Video v) {
+        try {
+            String sql = "UPDATE" + ConstantManager.DEFAULT_DB_NAME + ".video "
+                    + "SET id  =?, link =?, title =? "
+                    + "WHERE id =?";
+            jdbc.update(sql, v.getId(), v.getLink(), v.getTitle());
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Boolean.FALSE;
+        }
+    }
+
+    public Boolean updateStatus(Video vi) {
+        try {
+            Boolean status = Boolean.TRUE;
+            if (Objects.equals(vi.getActive(), Boolean.TRUE)) {
+                status = Boolean.FALSE;
+            }
+            String sql = "UPDATE video SET active = " + status + " WHERE id = '" + vi.getId() + "'";
+            jdbc.update(sql);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return Boolean.FALSE;
+        }
+    }
+
+    public List<Video> getLastInsert() {
+        String sql = "SELECT * FROM" + ConstantManager.DEFAULT_DB_NAME + ".video order by created_time DESC LIMIT 1;";
         return getBySql(sql);
     }
 }
--- src/java/com/poly/service/VideoService.java
+++ src/java/com/poly/service/VideoService.java
@@ -7,7 +7,10 @@ package com.poly.service;
 
 import com.poly.bean.Video;
 import com.poly.dao.VideoDAO;
+import com.poly.request.VideoRequest;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +20,45 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VideoService {
-      @Autowired
+
+    @Autowired
     private VideoDAO vDAO;
-public List<Video> getVideo(){
-    return vDAO.getVideo();
-}
-   
+
+    public List<Video> getVideo() {
+        return vDAO.getVideo();
+    }
+
+    public Boolean delete(String id) {
+        return !Objects.equals(vDAO.delete(id), Boolean.FALSE);
+    }
+
+    public Boolean insert(VideoRequest video, String id) {
+        UUID uuid = UUID.randomUUID();
+        Video vi = new Video();
+        vi.setId(uuid.toString());
+        vi.setLink(vi.getLink());
+        vi.setTitle(vi.getTitle());
+        if (Objects.equals(vDAO.insert(vi), Boolean.FALSE)) {
+            return Boolean.FALSE;
+        }
+        return Boolean.TRUE;
+    }
+
+    public Boolean update(VideoRequest video, String id) {
+        Video vi = new Video();
+        vi.setId(vi.getId());
+        vi.setLink(vi.getLink());
+        vi.setTitle(vi.getTitle());
+        return !Objects.equals(vDAO.update(vi), Boolean.FALSE);
+    }
+
+    public Boolean updateStatus(String id, Boolean status) {
+        Video vi = new Video();
+        vi.setActive(status);
+        vi.setId(id);
+        return !Objects.equals(vDAO.updateStatus(vi), Boolean.FALSE);
+    }
+    public List<Video> checkLastInsert() {
+        return vDAO.getLastInsert();
+    }
 }
